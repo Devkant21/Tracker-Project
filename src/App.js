a1b38abe3ebac0bed93cc1c45165fd48b9cb4a2d
Devkant <devkant21@gmail.com>
2022-01-20 13:29:39 +0530

    Dark mode switching

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+  color: ${props => props.theme.fontColor};
+`;
 
 function App() {
+
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}> 
+      <GlobalStyles />
+      <StyledApp>
+        SandPark
+        <button onClick={() => themeToggler()}>Dark / Light</button>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
